86f977fa87cfab1decac14dd76fc8629118d96d7
yassinChaa <yassin.chaabouni@esprit.tn>
2024-04-27 14:30:44 +0100

    merge

--- src/app/Models/type-terrain.spec.ts
+++
@@ -1,7 +0,0 @@
-import { TypeTerrain } from "./Terrain/terrain";
-
-describe('TypeTerrain', () => {
-  it('should create an instance', () => {
-    expect(new TypeTerrain()).toBeTruthy();
-  });
-});
